21c4f991573905be62612bd906eebb8fed854007
scriptprojectsdev <support@scriptprojects.com>
2020-05-25 17:49:30 -0500

    Handle an issue without a screenshot in webhook integration

--- Sources/Bugless/Integrations/WebhookIntegration.swift
+++ Sources/Bugless/Integrations/WebhookIntegration.swift
@@ -18,23 +18,30 @@ class WebhookIntegration: IssueIntegration {
         //Signal the beginning of submittion so loader can be shown
         NotificationCenter.default.post(name: .buglessIssueSubmittionStarted, object: nil)
         
-        NetworkHelper.imgurUpload(image: issue.screenshots[0], withCredentials: Bugless.configuration.imageServiceCredentials) { (imageLink) in
-            if let image = imageLink {
-                let issueToSend = SerializableIssue(issue, imageLinks: [image])
-                let payload = issueToSend.toJson()
-                NetworkHelper.postJson(self.webhookUrl, jsonString: payload) { (data, response, error) in
-                    if error == nil {
-                        //Dismiss feedback controller
-                        NotificationCenter.default.post(name: .buglessIssueSubmittionSuccesful, object: nil)
-                    } else {
-                        NotificationCenter.default.post(name: .buglessIssueSubmitionFailed, object: nil, userInfo: ["error": error!])
-                    }
-                }
+        if issue.screenshots.count > 0 {
+            //TODO: Support multiple screenshots
+            NetworkHelper.imgurUpload(image: issue.screenshots[0], withCredentials: Bugless.configuration.imageServiceCredentials) { (imageLink) in
+                self.send(issue: issue, with: imageLink)
             }
+        } else {
+            send(issue: issue, with: nil)
         }
     }
     
     static func doesRequireCredentials() -> Bool {
         return false
+    }
+    
+    func send(issue: Issue, with imageLink: String?) {
+        let issueToSend = SerializableIssue(issue, imageLinks: imageLink == nil ? [] : [imageLink!])
+        let payload = issueToSend.toJson()
+        NetworkHelper.postJson(self.webhookUrl, jsonString: payload) { (data, response, error) in
+            if error == nil {
+                //Dismiss feedback controller
+                NotificationCenter.default.post(name: .buglessIssueSubmittionSuccesful, object: nil)
+            } else {
+                NotificationCenter.default.post(name: .buglessIssueSubmitionFailed, object: nil, userInfo: ["error": error!])
+            }
+        }
     }
 }
