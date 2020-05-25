f25664f3b3195db32a41fe49826aad2dbabff04a
scriptprojectsdev <support@scriptprojects.com>
2020-05-25 17:50:13 -0500

    Added simple loader alert

--- Sources/Bugless/Helpers/AlertHelper.swift
+++ Sources/Bugless/Helpers/AlertHelper.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  AlertHelper.swift
 //  
 //
 //  Created By ScriptProjects, LLC on 4/16/20.
@@ -7,14 +7,31 @@
 
 import UIKit
 
-class AlertHelper {
+public class AlertHelper {
     
-    static func info(title: String, message: String, buttonTitle: String = "Dismiss", context: UIViewController) {
+    static var loaderController: UIAlertController?
+    
+    static func info(title: String, message: String, buttonTitle: String = "Dismiss", context: UIViewController, completion: ((UIAlertAction) -> Void)? = nil) {
         
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alert.addAction(.init(title: buttonTitle, style: .cancel, handler: nil))
-        context.present(alert, animated: true)
+        alert.addAction(.init(title: buttonTitle, style: .cancel, handler: completion))
+        DispatchQueue.main.async {
+            context.present(alert, animated: true)
+        }
         
+    }
+    
+    public static func showLoader(message: String = "Loading...", context: UIViewController) {
+        
+        hideLoader()
+        loaderController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        
+        loaderController?.view.addSubview(UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50)))
+        context.present(loaderController!, animated: true)
+    }
+    
+    public static func hideLoader() {
+        loaderController?.dismiss(animated: true)
     }
     
 }
